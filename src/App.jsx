6b878c1b82302aea4deadc3d12efdb0efd0566fc
lyhhh <17610603221@163.com>
2022-03-26 00:57:00 +0800

    init2

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,38 @@
-import React from 'react'
-import { connect, createStore, Provider } from './redux.jsx'
-import { connectToUser } from './connecters/connect-to-user'
+import { createContext, useContext, useState } from 'react'
 
-const reducer = (state, { type, payload }) => {
-  if (type === 'updateUser') {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...payload,
-      },
-    }
-  } else {
-    return state
-  }
-}
+const Context = createContext(null)
 
-const initState = {
-  user: { name: 'hi', age: 99 },
-  group: { name: 'this is a group' },
-}
-
-const store = createStore(reducer, initState)
-
-const ajax = (n = 3) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({ data: { name: `这是 ${n} 秒后` } })
-    }, n * 1000)
-  })
-}
-
-function App() {
+export default function App() {
+  const [appState, setAppState] = useState({ val: '' })
   return (
-    <Provider store={store}>
-      <FirstChild />
-      <SecondChild />
-      <LastChild />
-    </Provider>
+    <Context.Provider value={{ appState, setAppState }}>
+      <Section1 />
+      <Section2 />
+    </Context.Provider>
   )
 }
 
-const FirstChild = () => (
-  <section>
-    大儿子
-    <User />
-  </section>
-)
+const Section1 = () => {
+  const { appState, setAppState } = useContext(Context)
 
-const SecondChild = () => (
-  <section>
-    二儿子
-    <UserModifier />
-  </section>
-)
-
-const LastChild = connect(state => ({
-  group: state.group,
-}))(({ group }) => (
-  <section>
-    幺儿子: <span>{group.name}</span>
-  </section>
-))
-
-const User = connectToUser(({ user }) => {
-  return <div>User:{user.name}</div>
-})
-
-const fetchUser = async dispatch => {
-  const { data } = await ajax(2)
-  dispatch({ type: 'updateUser', payload: data })
-}
-
-const fetchUserPromise = () => {
-  return ajax(3).then(res => res.data)
-}
-
-const UserModifier = connect(
-  null,
-  null,
-)(({ state, dispatch }) => {
   return (
-    <div>
-      <div>User: {state.user.name}</div>
-      <button
-        onClick={() => {
-          dispatch(fetchUser)
-
-          dispatch({
-            type: 'updateUser',
-            payload: fetchUserPromise(),
-          })
-        }}
-      >
-        async test
-      </button>
-    </div>
+    <section>
+      <div>section1</div>
+      <div>
+        <input
+          placeholder="update section2"
+          onChange={e => setAppState({ ...appState, val: e.target.value })}
+        />
+      </div>
+    </section>
   )
-})
+}
 
-export default App
+const Section2 = () => {
+  const { appState } = useContext(Context)
+  return (
+    <section>
+      <div>section2: {appState.val}</div>
+    </section>
+  )
+}
--- src/connecters/connect-to-user.js
+++
@@ -1,8 +0,0 @@
-import { connect } from '../redux'
-
-const userSelector = state => ({ user: state.user })
-const userDispatcher = dispatch => ({
-  updateUser: attrs => dispatch({ type: 'updateUser', payload: attrs }),
-})
-
-export const connectToUser = connect(userSelector, userDispatcher)
